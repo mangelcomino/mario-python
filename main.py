e7d191415ceb03fe422d39f155e8d291524e89dd
miguel Angel <mangelcomino@gmail.com>
2023-04-01 15:11:35 +0200

    tiles

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame,sys
 from settings import *
+from tile import Tile
+
 #pygame setup
 pygame.init()
 
 screen = pygame.display.set_mode((screen_with,screen_height))
 clock = pygame.time.Clock()
 fps = 60
+test_tile = pygame.sprite.Group(Tile((100,100),200))
 
 while True:
     for event in pygame.event.get():
@@ -14,5 +17,7 @@ while True:
             sys.exit()
     screen.fill('black')
 
+    test_tile.draw(screen)
+    
     pygame.display.update()
     clock.tick(fps)
