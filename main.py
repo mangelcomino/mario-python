e12af3bb8eb1a817a3785424be4fb85fee2a89f6
miguel Angel <mangelcomino@gmail.com>
2023-04-01 13:03:31 +0200

    settings

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pygame,sys
-
+from settings import *
 #pygame setup
 pygame.init()
-screen_with = 1200
-screen_height = 700
+
 screen = pygame.display.set_mode((screen_with,screen_height))
 clock = pygame.time.Clock()
 fps = 60
